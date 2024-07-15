b5d118d5940dd1189ad3383d9fc7e07a6e3e3dd9
Mikko <realmiks27@gmail.com>
2024-07-15 16:16:28 -0700

    feat: changed tables into divs for styling purposes

--- frontend/src/routes/Service.tsx
+++ frontend/src/routes/Service.tsx
@@ -1,35 +1,30 @@
 import React from "react";
 
-
-// parameters will be the data
-// probably need to have a trains object corresponds to each object in the data
 export default function Service() {
     const lines = ["red", "orange", "yellow","green", "blue"]
 
     return (
         <div className="row-span-7 p-8">
-            <table className="border-black border-2">
-                <tbody>
-                    <tr className="border-black border-2">
-                        <th className="border-black border-2">Train</th>
-                        <th className="border-black border-2">Active</th>
-                        <th className="border-black border-2">Avg. wait</th>
-                        <th className="border-black border-2">Tardy (%)</th>
-                        <th className="border-black border-2">Avg. wait</th>
-                    </tr>
+            <div className="border-black border-2 ">
+                    <div className="border-black border-2 grid grid-cols-5">
+                        <div className=" font-bold">Train</div>
+                        <div className=" font-bold">Active</div>
+                        <div className=" font-bold">Avg. wait</div>
+                        <div className=" font-bold">Tardy (%)</div>
+                        <div className=" font-bold">Avg. wait</div>
+                    </div>
                     {lines.map((line, i) => {
                         return (
-                            <tr key={i} className={`border-black border-2 bg-${line}-400`}>
-                                <td className="border-black border-2">{line}</td>
-                                <td className="border-black border-2">hi1</td>
-                                <td className="border-black border-2">hi2</td>
-                                <td className="border-black border-2">hi3</td>
-                                <td className="border-black border-2">hi4</td>
-                            </tr>
+                            <div key={i} className={`border-black border-2 bg-${line}-400 grid grid-cols-5`}>
+                                <div className="">{line}</div>
+                                <div className="">hi1</div>
+                                <div className="">hi2</div>
+                                <div className="">hi3</div>
+                                <div className="">hi4</div>
+                            </div>
                         )
                     })}
-                </tbody>
-            </table>
+            </div>
         </div>
     )
 }
