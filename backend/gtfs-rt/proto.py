023698a24729ed77758a2fc6cb9d39c80d8af855
Alexander Chang <140032131+AlexanderChang140@users.noreply.github.com>
2024-07-16 21:19:29 +0000

    refactor: write to temporary directory

--- backend/gtfs-rt/proto.py
+++ backend/gtfs-rt/proto.py
@@ -1,16 +1,20 @@
 import requests
 import subprocess
 import os
+import tempfile
 
 def generate_proto_python_classes():
     url = "https://gtfs.org/realtime/gtfs-realtime.proto"
-    path = "gtfs-realtime.proto"
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-    subprocess.run(["protoc", "--python_out=.", "gtfs-realtime.proto"])
-    os.remove(path)
+    
+    with tempfile.TemporaryDirectory() as temp_dir:
+        path = os.path.join(temp_dir, "gtfs-realtime.proto")
+        
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(path, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        
+        subprocess.run(["protoc", "--proto_path=" + temp_dir, "--python_out=.", path])
 
 generate_proto_python_classes()
