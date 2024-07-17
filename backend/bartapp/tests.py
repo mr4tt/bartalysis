513e8ff6eef24382a2d76d61c52ed2631243ef81
Carly Thomas <112457607+CarlyAThomas@users.noreply.github.com>
2024-07-17 18:22:06 +0000

    Feat: Fixed route API routes, serializers, views, and models.

--- backend/bartapp/tests.py
+++ backend/bartapp/tests.py
@@ -1,133 +1,177 @@
-from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Agency, Route, Trip, Stop, Calendar, RealtimeStopTimeUpdate, RealtimeAlert, RealtimeTrip, trainschedule
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from .models import Agency
 
-class EndpointTestCase(TestCase):
+class AgencyDetailViewTests(APITestCase):
+    databases = {'default', 'bart'}
+
     def setUp(self):
-        self.client = Client()
+        # Set up data for the tests
+        self.client = APIClient()
+        self.test_agency = Agency.objects.create(agency_id="test_agency", agency_name="Test Agency Name")
+        self.url = reverse('agency-detail', kwargs={'agency_id': self.test_agency.agency_id})
 
-    def test_agency_list(self):
-        response = self.client.get(reverse('agency-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "agency_id")
-        self.assertContains(response, "agency_name")
+    def test_get_agency_detail(self):
+        # Test retrieving an agency by agency_id
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['agency_id'], self.test_agency.agency_id)
+        self.assertEqual(response.data['agency_name'], self.test_agency.agency_name)
 
-    def test_feed_info_list(self):
-        response = self.client.get(reverse('feedinfo-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "feed_publisher_name")
-        self.assertContains(response, "feed_lang")
+    def test_agency_not_found(self):
+        # Test retrieving an agency that does not exist
+        url = reverse('agency-detail', kwargs={'agency_id': 'nonexistent_agency'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_fare_attribute_list(self):
-        response = self.client.get(reverse('fareattribute-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "fare_id")
-        self.assertContains(response, "price")
+# from django.test import TestCase, Client
+# from django.urls import reverse
+# from .models import Agency #, Route, Trip, Stop, Calendar, RealtimeStopTimeUpdate, RealtimeAlert, RealtimeTrip#, trainschedule
 
-    def test_fare_rule_list(self):
-        response = self.client.get(reverse('farerule-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "fare_id")
-        self.assertContains(response, "route_id")
+# class EndpointTestCase(TestCase):
+#     databases = {'default', 'bart'}
+#     def setUp(self):
+#         self.client = Client()
+#         # Create a sample Agency instance for testing
+#         Agency.objects.create(agency_id="test_agency", agency_name="Test Agency")
 
-    def test_rider_category_list(self):
-        response = self.client.get(reverse('ridercategory-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "rider_category_id")
-        self.assertContains(response, "rider_category_description")
+#     def test_agency_list(self):
+#         response = self.client.get(reverse('agency-list'))
+#         self.assertEqual(response.status_code, 200)
+        
+#         # Check if the response is JSON and decode it
+#         if response['Content-Type'] == 'application/json':
+#             response_data = response.json()
+#             # Assuming the response should be a list of agencies, check if any agency contains 'agency_id'
+#             self.assertTrue(any('agency_id' in agency for agency in response_data), "Response does not contain 'agency_id'")
+#         else:
+#             # If the response is not JSON, directly search for 'agency_id' in the response content
+#             self.assertContains(response, "agency_id")
 
-    def test_fare_rider_category_list(self):
-        response = self.client.get(reverse('fareridercategory-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "fare_id")
-        self.assertContains(response, "rider_category_id")
+    # def test_agency_list(self):
+    #     response = self.client.get(reverse('agency-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "agency_id")
+    #     self.assertContains(response, "agency_name")
 
-    def test_shape_list(self):
-        response = self.client.get(reverse('shape-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "shape_id")
-        self.assertContains(response, "shape_pt_lat")
+    # def test_feed_info_list(self):
+    #     response = self.client.get(reverse('feedinfo-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "feed_publisher_name")
+    #     self.assertContains(response, "feed_lang")
 
-    def test_route_list(self):
-        response = self.client.get(reverse('route-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "route_id")
-        self.assertContains(response, "route_long_name")
+    # def test_fare_attribute_list(self):
+    #     response = self.client.get(reverse('fareattribute-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "fare_id")
+    #     self.assertContains(response, "price")
 
-    def test_route_attribute_list(self):
-        response = self.client.get(reverse('routeattribute-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "route_id")
-        self.assertContains(response, "category")
+    # def test_fare_rule_list(self):
+    #     response = self.client.get(reverse('farerule-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "fare_id")
+    #     self.assertContains(response, "route_id")
 
-    def test_realtime_route_list(self):
-        response = self.client.get(reverse('realtimeroute-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "route_id")
-        self.assertContains(response, "realtime_enabled")
+    # def test_rider_category_list(self):
+    #     response = self.client.get(reverse('ridercategory-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "rider_category_id")
+    #     self.assertContains(response, "rider_category_description")
 
-    def test_direction_list(self):
-        response = self.client.get(reverse('direction-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "route_id")
-        self.assertContains(response, "direction_id")
+    # def test_fare_rider_category_list(self):
+    #     response = self.client.get(reverse('fareridercategory-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "fare_id")
+    #     self.assertContains(response, "rider_category_id")
 
-    def test_stop_list(self):
-        response = self.client.get(reverse('stop-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "stop_id")
-        self.assertContains(response, "stop_name")
+    # def test_shape_list(self):
+    #     response = self.client.get(reverse('shape-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "shape_id")
+    #     self.assertContains(response, "shape_pt_lat")
 
-    def test_stop_time_list(self):
-        response = self.client.get(reverse('stoptime-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "trip_id")
-        self.assertContains(response, "stop_id")
+    # def test_route_list(self):
+    #     response = self.client.get(reverse('route-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "route_id")
+    #     self.assertContains(response, "route_long_name")
 
-    def test_transfer_list(self):
-        response = self.client.get(reverse('transfer-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "from_stop_id")
-        self.assertContains(response, "to_stop_id")
+    # def test_route_attribute_list(self):
+    #     response = self.client.get(reverse('routeattribute-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "route_id")
+    #     self.assertContains(response, "category")
 
-    def test_calendar_list(self):
-        response = self.client.get(reverse('calendar-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "service_id")
-        self.assertContains(response, "monday")
+    # def test_realtime_route_list(self):
+    #     response = self.client.get(reverse('realtimeroute-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "route_id")
+    #     self.assertContains(response, "realtime_enabled")
 
-    def test_calendar_attribute_list(self):
-        response = self.client.get(reverse('calendarattribute-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "service_id")
-        self.assertContains(response, "service_description")
+    # def test_direction_list(self):
+    #     response = self.client.get(reverse('direction-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "route_id")
+    #     self.assertContains(response, "direction_id")
 
-    def test_calendar_date_list(self):
-        response = self.client.get(reverse('calendardate-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "service_id")
-        self.assertContains(response, "date")
+    # def test_stop_list(self):
+    #     response = self.client.get(reverse('stop-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "stop_id")
+    #     self.assertContains(response, "stop_name")
 
-    def test_trip_list(self):
-        response = self.client.get(reverse('trip-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "trip_id")
-        self.assertContains(response, "route_id")
+    # def test_stop_time_list(self):
+    #     response = self.client.get(reverse('stoptime-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "trip_id")
+    #     self.assertContains(response, "stop_id")
 
-    def test_rt_stop_time_update_list(self):
-        response = self.client.get(reverse('rtstoptimeupdate-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "trip_id")
-        self.assertContains(response, "stop_id")
+    # def test_transfer_list(self):
+    #     response = self.client.get(reverse('transfer-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "from_stop_id")
+    #     self.assertContains(response, "to_stop_id")
 
-    def test_rt_alert_list(self):
-        response = self.client.get(reverse('rtalert-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "alert_id")
-        self.assertContains(response, "info")
+    # def test_calendar_list(self):
+    #     response = self.client.get(reverse('calendar-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "service_id")
+    #     self.assertContains(response, "monday")
 
-    def test_rt_trip_list(self):
-        response = self.client.get(reverse('rttrip-list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "trip_id")
-        self.assertContains(response, "schedule_relationship")
+    # def test_calendar_attribute_list(self):
+    #     response = self.client.get(reverse('calendarattribute-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "service_id")
+    #     self.assertContains(response, "service_description")
+
+    # def test_calendar_date_list(self):
+    #     response = self.client.get(reverse('calendardate-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "service_id")
+    #     self.assertContains(response, "date")
+
+    # def test_trip_list(self):
+    #     response = self.client.get(reverse('trip-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "trip_id")
+    #     self.assertContains(response, "route_id")
+
+    # def test_rt_stop_time_update_list(self):
+    #     response = self.client.get(reverse('rtstoptimeupdate-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "trip_id")
+    #     self.assertContains(response, "stop_id")
+
+    # def test_rt_alert_list(self):
+    #     response = self.client.get(reverse('rtalert-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "alert_id")
+    #     self.assertContains(response, "info")
+
+    # def test_rt_trip_list(self):
+    #     response = self.client.get(reverse('rttrip-list'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "trip_id")
+    #     self.assertContains(response, "schedule_relationship")
--- backend/bartapp/views.py
+++ backend/bartapp/views.py
@@ -1,35 +1,75 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.http import HttpResponse
 import requests
+
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.decorators import api_view
 
 from rest_framework import viewsets
 from .models import (
     Agency, 
     FeedInfo,
+    FareAttribute,
+    FareRule,
+    RiderCategory,
+    FareRiderCategory,
     Route, 
     Stop, 
-    StopTime, 
-    Calendar, 
-    Trip, 
+    Trip,
+    Calendar,
+    RouteAttribute,
+    RealtimeRoute,
+    Direction,
+    StopTime,
+    Transfer,
+    Shape,
+    CalendarAttribute,
+    CalendarDate,
+    RealtimeStopTimeUpdate,
+    RealtimeAlert,
+    RealtimeTrip,
     TrainSchedule,
-    RealtimeStopTimeUpdate
 )
 
 from .serializers import (
     AgencySerializer, 
     FeedInfoSerializer,
-    RouteSerializer, 
+    FareAttributeSerializer,
+    FareRuleSerializer,
+    RiderCategorySerializer,
+    FareRiderCategorySerializer,
+    ShapeSerializer,
+    RouteSerializer,
+    RouteAttributeSerializer,
+    RealtimeRouteSerializer,
+    DirectionSerializer, 
     StopSerializer, 
-    StopTimeSerializer, 
-    CalendarSerializer, 
+    StopTimeSerializer,
+    TransferSerializer,
+    CalendarSerializer,
+    CalendarAttributeSerializer,
+    CalendarDateSerializer, 
     TripSerializer, 
-    RealtimeStopTimeUpdateSerializer
+    RealtimeStopTimeUpdateSerializer,
+    RealtimeAlertSerializer,
+    RealtimeTripSerializer,
+    TrainScheduleSerializer,
 )
+
+from rest_framework import generics
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the BART app home page.")
 
 # Deprecated
 def get_departures(request):
@@ -44,6 +84,30 @@ def get_agencies(request):
     agencies = Agency.objects.all()
     return render(request, 'agency_list.html', {'agencies': agencies})
 
+class AgencyViewSet(viewsets.ModelViewSet):
+    queryset = Agency.objects.all()
+    serializer_class = AgencySerializer
+
+class FeedInfoViewSet(viewsets.ModelViewSet):
+    queryset = FeedInfo.objects.all()
+    serializer_class = FeedInfoSerializer
+
+class FareAttributeViewSet(viewsets.ModelViewSet):
+    queryset = FareAttribute.objects.all()
+    serializer_class = FareAttributeSerializer
+
+class FareRuleViewSet(viewsets.ModelViewSet):
+    queryset = FareRule.objects.all()
+    serializer_class = FareRuleSerializer
+
+class RiderCategoryViewSet(viewsets.ModelViewSet):
+    queryset = RiderCategory.objects.all()
+    serializer_class = RiderCategorySerializer
+
+class FareRiderCategoryViewSet(viewsets.ModelViewSet):
+    queryset = FareRiderCategory.objects.all()
+    serializer_class = FareRiderCategorySerializer
+
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
@@ -56,27 +120,71 @@ class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
 
-class StopTimeViewSet(viewsets.ModelViewSet):
-    queryset = StopTime.objects.all()
-    serializer_class = StopTimeSerializer
-
-class AgencyViewSet(viewsets.ModelViewSet):
-    queryset = Agency.objects.all()
-    serializer_class = AgencySerializer
-
 class CalendarViewSet(viewsets.ModelViewSet):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
 
-class FeedInfoViewSet(viewsets.ModelViewSet):
-    queryset = FeedInfo.objects.all()
-    serializer_class = FeedInfoSerializer
+class RouteAttributeViewSet(viewsets.ModelViewSet):
+    queryset = RouteAttribute.objects.all()
+    serializer_class = RouteAttributeSerializer
 
-class RealtimeStopTimeUpdateView(APIView):
-    def get(self, request):
-        updates = RealtimeStopTimeUpdate.objects.all()
-        serializer = RealtimeStopTimeUpdateSerializer(updates, many=True)
-        return Response(serializer.data)
+class RealtimeRouteViewSet(viewsets.ModelViewSet):
+    queryset = RealtimeRoute.objects.all()
+    serializer_class = RealtimeRouteSerializer
+
+class DirectionViewSet(viewsets.ModelViewSet):
+    queryset = Direction.objects.all()
+    serializer_class = DirectionSerializer
+
+class StopTimeViewSet(viewsets.ModelViewSet):
+    queryset = StopTime.objects.all()
+    serializer_class = StopTimeSerializer
+
+class TransferViewSet(viewsets.ModelViewSet):
+    queryset = Transfer.objects.all()
+    serializer_class = TransferSerializer
+
+class ShapeViewSet(viewsets.ModelViewSet):
+    queryset = Shape.objects.all()
+    serializer_class = ShapeSerializer
+
+class CalendarAttributeViewSet(viewsets.ModelViewSet):
+    queryset = CalendarAttribute.objects.all()
+    serializer_class = CalendarAttributeSerializer
+
+class CalendarDateViewSet(viewsets.ModelViewSet):
+    queryset = CalendarDate.objects.all()
+    serializer_class = CalendarDateSerializer
+
+class RealtimeStopTimeUpdateViewSet(viewsets.ModelViewSet):
+    queryset = RealtimeStopTimeUpdate.objects.all()
+    serializer_class = RealtimeStopTimeUpdateSerializer
+
+    # def get(self, request, stop_id):
+    #     realtime_stop_times = RealtimeStopTimeUpdate.objects.filter(stop_id=stop_id)
+    #     serializer = RealtimeStopTimeUpdateSerializer(realtime_stop_times, many=True)
+    #     return Response(serializer.data)
+
+class RealtimeAlertViewSet(viewsets.ModelViewSet):
+    queryset = RealtimeAlert.objects.all()
+    serializer_class = RealtimeAlertSerializer
+
+class RealtimeTripViewSet(viewsets.ModelViewSet):
+    queryset = RealtimeTrip.objects.all()
+    serializer_class = RealtimeTripSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class IncomingTrainsView(APIView):
     def incoming_trains_view(request, station_id):
@@ -93,3 +201,6 @@ class IncomingTrainsView(APIView):
             station_id=station_id,
             arrival_time__gte=user_time
         ).order_by('arrival_time')[:3]
+
+        serializer = TrainScheduleSerializer(incoming_trains, many=True)
+        return Response(serializer.data)
