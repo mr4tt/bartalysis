5368f5c132948671e956ddbe19164e51f47d5322
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-07-19 18:35:57 +0000

    feat: add direction/line between two points on map

--- frontend/src/routes/GoogleMap.tsx
+++ frontend/src/routes/GoogleMap.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
-import { useEffect } from 'react';
+import { AdvancedMarker, APIProvider, Map, useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
+import { useEffect, useState } from 'react';
 import { stationList } from '../utils/stations';
 
 export default function GoogleMap() {
   const position = { lat: 37.668819, lng: -122.080795}
 
+  // watch the first vid in the google maps playlist to learn how to create the information bubble on each marker
   return (
     <div className="h-screen w-full">
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
@@ -15,7 +16,8 @@ export default function GoogleMap() {
         zoom={10}
         mapId={import.meta.env.VITE_MAP_ID}
       >
-        <Markers points={stationList}/>
+        {/* <Markers points={stationList}/> */}
+        <Directions />
       </Map>
     </APIProvider>
     </div>
@@ -35,8 +37,38 @@ const Markers = ({ points }: any) => {
             <span>🚂</span>
           </AdvancedMarker>
         )
-        
       })}
     </>
   )
+}
+
+const Directions = () => {
+  const map = useMap()
+  const routesLibrary = useMapsLibrary("routes")
+  const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>()
+  const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>()
+  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([])
+
+  useEffect(() => {
+    if (!routesLibrary || !map) return
+    setDirectionsService(new routesLibrary.DirectionsService())
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }))
+  }, [routesLibrary, map])
+
+  useEffect(() => {
+    if (!directionsService || !directionsRenderer) return
+
+    directionsService.route({
+      origin: "1620 Berryessa Road",
+      destination: "500 John Daly Blvd.",
+      travelMode: google.maps.TravelMode.DRIVING,
+      provideRouteAlternatives: true,
+    }).then((response) => {
+      directionsRenderer.setDirections(response)
+      setRoutes(response.routes)
+    })
+  }, [directionsService, directionsRenderer])
+
+  console.log(routes)
+  return (null)
 }
