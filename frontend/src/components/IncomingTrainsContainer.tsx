9c487e3126c4fd6c33aa8aaa4313c8435702337e
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-07-24 17:29:23 +0000

    fix: change wording

--- frontend/src/components/IncomingTrainsContainer.tsx
+++ frontend/src/components/IncomingTrainsContainer.tsx
@@ -6,7 +6,7 @@ export default function IncomingTrainsContainer({ train }: any) {
             <p>Train color: {train.TrainName.slice(0, train.TrainName.length - 2)}</p>
             <div className="flex justify-between">
                 <p>Incoming at: <span className="font-bold">{train.DepartureTime}</span></p>
-                <p>Departure: <span className="font-bold">{train.ArrivalTime}</span></p>
+                <p>ETA: <span className="font-bold">{train.ArrivalTime}</span></p>
             </div>
             <p>{train.TrainDescription}</p>
         </div>
