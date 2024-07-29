9ec6b9146694861365265d3cf11a2b11b1b0524d
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-07-29 17:42:33 +0000

    fix: remove /map route

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -7,7 +7,6 @@ export default function Nav() {
         <div className=" row-span-1 bg-slate-400 w-screen flex items-center justify-between px-8 py-4">
             <Link to="/" className="hover:underline">Home</Link>
             <Link to="/service" className="hover:underline">Service</Link>
-            <Link to="/map" className="hover:underline">Map</Link>
             <div>Logo placeholder</div>
         </div>
     )
