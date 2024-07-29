6d96bff5852da81c78a17c2942b97da79905e6e1
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-07-29 19:01:44 +0000

    feat: add UI tests for Nav.tsx

---
+++ frontend/tests/Nav.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { describe, it, expect, beforeEach, } from 'vitest';
+import "@testing-library/jest-dom"
+import Nav from "../src/components/Nav"
+import { Router } from 'react-router-dom';
+import {  createMemoryHistory } from 'history';
+
+describe("Nav.tsx", () => {
+    const history = createMemoryHistory();
+    beforeEach(() => {
+        render(
+            <Router location={history.location} navigator={history}>
+                <Nav />
+            </Router>
+        )
+    })
+
+    it("should contain Home link and work", () => {
+        fireEvent.click(screen.getByText(/Home/i));
+        expect(history.location.pathname).toBe('/');
+        expect(screen.getByText("Home")).toBeInTheDocument()
+        expect(screen.getByText("Home")).toHaveTextContent(/Home/i)
+    })
+
+    it("should contain Service link and work", () => {
+        fireEvent.click(screen.getByText(/Service/i));
+        expect(history.location.pathname).toBe('/service');
+        expect(screen.getByText("Service")).toBeInTheDocument()
+        expect(screen.getByText("Service")).toHaveTextContent(/Service/i)
+    })
+})
