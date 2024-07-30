5389f67e48a8e2a2a2e6d0e1bcffefacb8d8d6d0
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-07-30 17:21:53 +0000

    fix: use userEvent instead of fireEvent

--- frontend/tests/Nav.test.tsx
+++ frontend/tests/Nav.test.tsx
@@ -1,13 +1,16 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { describe, it, expect, beforeEach, } from 'vitest';
 import "@testing-library/jest-dom"
 import Nav from "../src/components/Nav"
 import { Router } from 'react-router-dom';
 import {  createMemoryHistory } from 'history';
+import userEvent from "@testing-library/user-event";
 
 describe("Nav.tsx", () => {
     const history = createMemoryHistory();
+    const user = userEvent.setup();
+
     beforeEach(() => {
         render(
             <Router location={history.location} navigator={history}>
@@ -16,15 +19,15 @@ describe("Nav.tsx", () => {
         )
     })
 
-    it("should contain Home link and work", () => {
-        fireEvent.click(screen.getByText(/Home/i));
+    it("should contain Home link and work", async() => {
+        await user.click(screen.getByText(/Home/i));
         expect(history.location.pathname).toBe('/');
         expect(screen.getByText("Home")).toBeInTheDocument()
         expect(screen.getByText("Home")).toHaveTextContent(/Home/i)
     })
 
-    it("should contain Service link and work", () => {
-        fireEvent.click(screen.getByText(/Service/i));
+    it("should contain Service link and work", async() => {
+        await user.click(screen.getByText(/Service/i));
         expect(history.location.pathname).toBe('/service');
         expect(screen.getByText("Service")).toBeInTheDocument()
         expect(screen.getByText("Service")).toHaveTextContent(/Service/i)
