f27d17633b37cca1e4bd774155cb530fe822bac2
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-07-30 20:53:55 +0000

    feat: add a UI test to for RoutePlanner

--- frontend/tests/RoutePlanner.test.tsx
+++ frontend/tests/RoutePlanner.test.tsx
@@ -3,19 +3,36 @@ import RoutePlanner from "../src/routes/RoutePlanner"
 import React from 'react';
 import { describe, it, expect } from 'vitest';
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom"
 
 describe("RoutePlanner", () => {
     it("renders RoutePlanner", () => {
-        // since screen does not have the container property, we'll destructure render to obtain a container for this test
         const { container } = render(<RoutePlanner />);
         expect(container).toMatchSnapshot();
     });
 
-    // it("checks user interactions", async() => {
-    //     const user = userEvent.setup()
-    //     render(<RoutePlanner />)
-    //     const originForm = screen.getByRole("combobox", { name: "Select"})
-    //     await user.selectOptions(originForm, ["Fremont"]); 
+    it("fare price should be displayed", async() => {
+        const fare = {
+            "Description": "Clipper START",
+            "FareID": 2394,
+            "Price": 4.7
+        }
+        render(<RoutePlanner />)
+        const user = userEvent.setup()
+        const origin = screen.getByLabelText(/origin/i) as HTMLSelectElement;
+        const destination = screen.getByLabelText(/destination/i) as HTMLSelectElement;
+        const submitBtn = screen.getByRole('button', { name: /submit/i });
 
-    // })
+        origin.selectedIndex = 9;
+        origin.dispatchEvent(new Event('change'));
+
+        destination.selectedIndex = 15;
+        destination.dispatchEvent(new Event('change'));
+        // await user.click(submitBtn)
+        
+        expect(origin).toBeInTheDocument()
+        expect(destination).toBeInTheDocument()
+        // expect(screen.getByText(/Clipper START/i)).toBeInTheDocument()
+
+    })
 })
