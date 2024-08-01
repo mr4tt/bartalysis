a23a68741e359cca528467cc739a0a8073beb681
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-08-01 18:19:08 +0000

    feat: add color and refactor layout

--- frontend/src/components/IncomingTrainsContainer.tsx
+++ frontend/src/components/IncomingTrainsContainer.tsx
@@ -3,7 +3,7 @@ import { train } from "../utils/types";
 
 export default function IncomingTrainsContainer({ train }: { train: train }) {
     return (
-        <div className="bg-slate-500 px-4 py-2 grid gap-2">
+        <div className="bg-slate-300 px-4 py-2 grid gap-2">
             <p>{train.TrainDescription}</p>
             <p>Train color: {train.TrainName.slice(0, train.TrainName.length - 2)}</p>
             <div className="flex justify-between">
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
 
     return (
-        <div className=" row-span-1 bg-slate-400 w-screen flex items-center justify-between px-8 py-4">
+        <div className=" bg-blue-300 text-black row-span-1 w-screen flex items-center justify-between px-8 py-4">
             <Link to="/" className="hover:underline">Home</Link>
             <Link to="/service" className="hover:underline">Service</Link>
             <div>Logo placeholder</div>
