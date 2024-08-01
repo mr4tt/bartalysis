2c2ca34e4647d08a2cfa4e537cab485fcb1e2b43
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-08-01 18:26:09 +0000

    fix: change layout and size of Nav text

--- frontend/src/components/IncomingTrainsContainer.tsx
+++ frontend/src/components/IncomingTrainsContainer.tsx
@@ -6,7 +6,7 @@ export default function IncomingTrainsContainer({ train }: { train: train }) {
         <div className="bg-slate-300 px-4 py-2 grid gap-2">
             <p>{train.TrainDescription}</p>
             <p>Train color: {train.TrainName.slice(0, train.TrainName.length - 2)}</p>
-            <div className="flex justify-between">
+            <div className="flex justify-between items-center">
                 <p>Incoming at: <span className="font-bold">{train.DepartureTime}</span></p>
                 <p>ETA: <span className="font-bold">{train.ArrivalTime}</span></p>
             </div>
