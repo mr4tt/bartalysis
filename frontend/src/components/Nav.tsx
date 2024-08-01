4280d5cf4c4307ab0b679e168235055436a1f72f
JohnMikko27 <54955894+JohnMikko27@users.noreply.github.com>
2024-08-01 18:26:09 +0000

    fix: change layout and size of Nav text

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
 
     return (
-        <div className=" bg-blue-300 text-black row-span-1 w-screen flex items-center justify-between px-8 py-4">
-            <Link to="/" className="hover:underline">Home</Link>
-            <Link to="/service" className="hover:underline">Service</Link>
-            <div>Logo placeholder</div>
+        <div className=" bg-blue-300 text-black row-span-1 w-screen flex items-center justify-between px-8 py-4 text-xl">
+            <div className="justify-self-start flex gap-24">
+                <Link to="/" className="hover:underline">Home</Link>
+                <Link to="/service" className="hover:underline">Service</Link>
+
+            </div>
+            <div className="">Logo placeholder</div>
         </div>
     )
 }
